a34a64e1b2d428e0cb42aa915a34485d10a801d9
jfzara <zarajeanfabrice@gmail.com>
2023-01-17 08:28:06 -0500

    created Darko and Ramos: property arrays remain to be filled with expected values

--- Desktop/2023workspace/JamBro/app.js
+++ Desktop/2023workspace/JamBro/app.js
@@ -1,13 +1,16 @@
-let Bro = {
-    name:,
-    color:,
-    avatar:,
-    mood:,
-    favorite_sounds:[],
-    favorite bpm range:,
-    phrases:,
-    MBTI:,
+
+
+function Bro(name, color, avatar, mood, favorite_sounds, favorite_bpms, phrases, MBTI) {
+    this.name = name;
+    this.color = color;
+    this.avatar = avatar;
+    this.mood = mood;
+    this.favorite_sounds = favorite_sounds;
+    this.favorite_bpms = favorite_bpms;
+    this.phrases = phrases;
+    this.MBTI = MBTI;
+
 }
 
-
-
+let Darko = new Bro("Darko", "black", "", [], [],[],[], "");
+let Ramos = new Bro("Ramos", "black", "", [], [],[],[], "");
