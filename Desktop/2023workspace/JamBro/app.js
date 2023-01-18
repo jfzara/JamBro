f26e221f276430a3807c9f455aa430ec0f1ecc04
jfzara <zarajeanfabrice@gmail.com>
2023-01-18 07:34:04 -0500

    started server

--- Desktop/2023workspace/JamBro/app.js
+++ Desktop/2023workspace/JamBro/app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('HelloWOrld!');
+    res.send('HelloWorld!');
 });
 app.listen(3000, () => console.log('Listening on port 3000'));
 
