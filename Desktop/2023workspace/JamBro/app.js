936a02676ef335810ae0419e343920c77e64e41b
jfzara <zarajeanfabrice@gmail.com>
2023-01-18 07:23:14 -0500

    started server on port 3000

--- Desktop/2023workspace/JamBro/app.js
+++ Desktop/2023workspace/JamBro/app.js
@@ -1,5 +1,15 @@
 
 
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('HelloWOrld!');
+});
+app.listen(3000, () => console.log('Listening on port 3000'));
+
+
+
 function Bro(name, color, avatar, mood, favorite_sounds, favorite_bpms, phrases, favorite_genres) {
     this.name = name;
     this.color = color;
@@ -12,8 +22,8 @@ function Bro(name, color, avatar, mood, favorite_sounds, favorite_bpms, phrases,
 
 }
 
-let Darko = new Bro("Darko", "black", "", 
-["low frequency sound", "dark sound", "darker sound", "low pad sound","moody pad sound", "soft and dark sound", "low frequency percussions", "menacing pad sounds", "sinister synth sound                                                                                                                                                                                         v "], [],[],[], []);
-let Ramos = new Bro("Ramos", "red", "", [], [],[],[], []);
+let Darko = new Bro('Darko', 'black', '',
+    ['low frequency sound', 'dark sound', 'darker sound', 'low pad sound', 'moody pad sound', 'soft and dark sound', 'low frequency percussions', 'menacing pad sounds', 'sinister synth sound                                                                                                                                                                                         v '], [], [], [], []);
+let Ramos = new Bro('Ramos', 'red', '', [], [], [], [], []);
 //avatars are the Url s of each image or each image should be made with CSS styling
 //moods can be store in some object maybe and called  randomly when needed
