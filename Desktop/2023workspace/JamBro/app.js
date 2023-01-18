9888086354da764071f6a7a4282a97a61269eee7
jfzara <zarajeanfabrice@gmail.com>
2023-01-18 11:55:34 -0500

    created all Bro properties, bug to be detected

--- Desktop/2023workspace/JamBro/app.js
+++ Desktop/2023workspace/JamBro/app.js
@@ -3,18 +3,8 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('HelloWorld!');
-});
-app.listen(3000, () => console.log('Listening on port 3000'));
-
-app.get('/api/bros', (req, res) => {
-    res.send ([1, 2, 3]);
-})
-
-
-
-function Bro(name, color, avatar, mood, favorite_sounds, favorite_bpms, phrases, favorite_genres) {
+function Bro (id, name, color, avatar, mood, favorite_sounds, favorite_bpms, favorite_genres, phrases) {
+    this.id = id;
     this.name = name;
     this.color = color;
     this.avatar = avatar;
@@ -23,11 +13,60 @@ function Bro(name, color, avatar, mood, favorite_sounds, favorite_bpms, phrases,
     this.favorite_bpms = favorite_bpms;
     this.favorite_genres = favorite_genres;
     this.phrases = phrases;
+};
 
-}
 
-let Darko = new Bro('Darko', 'black', '',
-    ['low frequency sound', 'dark sound', 'darker sound', 'low pad sound', 'moody pad sound', 'soft and dark sound', 'low frequency percussions', 'menacing pad sounds', 'sinister synth sound                                                                                                                                                                                         v '], [], [], [], []);
-let Ramos = new Bro('Ramos', 'red', '', [], [], [], [], []);
+
+
+
+
+
+
+
+
+
+
+const Darko = new Bro('1', 'Darko', 'black', 'avatarDarko', 'dark',
+    ['low frequency sound', 'dark sound',
+        'darker sound', 'low pad sound',
+        'moody pad sound', 'soft and dark sound',
+        'low frequency percussions', 'menacing pad sounds',
+        'sinister synth sound'], [81, 100, 113, 85, 102, 76], ['ballad', 'gothic', 'cold wave'],
+    ['This needs some more mystery', 'Even darker', 'Darker', 'Something sinister']);                                                                                                                                                                               v '], [], [], [], []);
+
+
+const Ramos = new Bro('2', 'Ramos', 'red', 'avatarRamos', 'fiesta', ['bright sound', 'horn stab',
+    'exotic percussion sound', 'triangle sound',
+    'brass sound', 'bright lead', 'vocal stab sound'], [120, 124, 123, 126], ['salsa', 'samba', 'afrobeat', 'ska', 'tropical house'],
+    ['Caramba!',
+        'Ay, more percussions!', 'Make it more caliente!']);
+
+
+
 //avatars are the Url s of each image or each image should be made with CSS styling
 //moods can be store in some object maybe and called  randomly when needed
+
+const bros = [
+
+];
+
+app.get('/', (req, res) => {
+    res.send('HelloWorld!');
+});
+app.listen(3000, () => console.log(`Listening on ${port}`));
+
+app.get('/api/bros', (req, res) => {
+    res.send([1, 2, 3]);
+})
+
+app.get('/api/bros/:id', (req, res) => {
+    const bro = bros.find(br => br.id === parseInt(req.params.id));
+});//on met /:id pour chaque parametre, on peut mettre plusieurs parametres /:id/:foo/:bar etc
+
+
+
+const port = process.env.PORT || 3000;
+
+
+
+
