1561f57ea49a908108e6a408eab6b19ba22f51f8
jfzara <zarajeanfabrice@gmail.com>
2023-01-18 07:04:38 -0500

    installed epress + dependencies

--- Desktop/2023workspace/JamBro/app.js
+++ Desktop/2023workspace/JamBro/app.js
@@ -1,16 +1,19 @@
 
 
-function Bro(name, color, avatar, mood, favorite_sounds, favorite_bpms, phrases, MBTI) {
+function Bro(name, color, avatar, mood, favorite_sounds, favorite_bpms, phrases, favorite_genres) {
     this.name = name;
     this.color = color;
     this.avatar = avatar;
     this.mood = mood;
     this.favorite_sounds = favorite_sounds;
     this.favorite_bpms = favorite_bpms;
+    this.favorite_genres = favorite_genres;
     this.phrases = phrases;
-    this.MBTI = MBTI;
 
 }
 
-let Darko = new Bro("Darko", "black", "", [], [],[],[], "");
-let Ramos = new Bro("Ramos", "black", "", [], [],[],[], "");
+let Darko = new Bro("Darko", "black", "", 
+["low frequency sound", "dark sound", "darker sound", "low pad sound","moody pad sound", "soft and dark sound", "low frequency percussions", "menacing pad sounds", "sinister synth sound                                                                                                                                                                                         v "], [],[],[], []);
+let Ramos = new Bro("Ramos", "red", "", [], [],[],[], []);
+//avatars are the Url s of each image or each image should be made with CSS styling
+//moods can be store in some object maybe and called  randomly when needed
